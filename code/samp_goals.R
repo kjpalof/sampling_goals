2b6486715c0415c981a5d79bd07c2de91a56ee13
Katie Palof <katie.palof@alaska.gov>
2017-01-04 11:25:58 -0900

    initial commit

---
+++ code/samp_goals.R
@@ -0,0 +1,64 @@
+#K.Palof ADF&G
+#objective: determin how many trips to sample for lenghts in the crab fisheries (golden, tanner, 
+#         and dungeness - seperately) to be able to tell if the % of recruits changes from year to year.
+#statistical objective: 
+#power analysis to determine the number of trips to sample from each management area
+#         to detect
+
+rm(list = ls()) # clear workspace since data frames have same names
+#####Load Packages ---------------------------------
+library(tidyverse)
+library(pwr)
+library(broom)
+library(purrr)
+#####Load Data ---------------------------------------------------
+# change input file for each
+dat <- read.csv("./data/gkc_dockside_13_16.csv")
+
+#unique(dat$SEASON) you season NOT year
+dat %>%
+  group_by(SEASON, I_FISHERY, TRIP_NO, RECRUIT_STATUS) %>%
+  summarise(N = n()) -> by_trip
+
+by_trip %>%
+  group_by(SEASON, I_FISHERY, TRIP_NO)%>%
+  summarise(tot_crab = sum(N)) -> total_by_trip
+
+by_trip %>%
+  left_join(total_by_trip) %>%
+  mutate(percent = N/tot_crab) -> by_trip2
+
+by_trip2 %>%
+  filter(RECRUIT_STATUS == "Recruit") ->recruit_by_trip2
+# summarize by season and fishery area
+#       these match those calculated in .JMP files in previous years
+recruit_by_trip2 %>%
+  group_by(SEASON, I_FISHERY) %>%
+  summarise(ntrip = n(), mean_percentR = mean(percent), sd_percentR = sd(percent)) -> recruits
+
+recruits %>%
+  filter(SEASON == "Oct2015 - Sep16")
+
+# Power analysis 
+# t-test - one sample
+tidy(pwr.t.test( d= (0.10/0.164196), sig.level = 0.05, power = 0.85, type = "one.sample", alternative = "two.sided"))
+
+# d is difference to detect which is the difference / S.D.
+
+# using dplyr and broom
+recruits %>% 
+  na.omit() %>%
+  mutate( value = crab_power(a = 0.10, b = sd_percentR, signif = 0.05, power = 0.85))
+
+# writing a function
+crab_power <- function(a, b, signif, power){
+  step1 <- pwr.t.test(d=(a/b), sig.level = signif, power = power, type = "one.sample", alternative = "two.sided")
+  step1$n
+}
+crab_power(0.10, recruits$sd_percentR, 0.05, 0.85)
+
+apply(recruits$sd_percentR, 1, crab_power(a =0.10, b = sd_percentR, signif = 0.05, power = 0.85))
+
+by_row(recruits, crab_power, a=0.10, signif = 0.05, power = 0.85, .collate = "list")
+
+head(recruits)
